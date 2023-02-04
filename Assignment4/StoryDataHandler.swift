1aec7cada710d6e17fb8fd70a311df0d8090f116
Brad Payne <brad@thepaynefamily.org>
2023-02-04 16:13:20 -0600

    -  Added Start function and other little testing functionality.

--- Assignment4/StoryDataHandler.swift
+++ Assignment4/StoryDataHandler.swift
@@ -9,22 +9,60 @@ import Foundation
 
 class   StoryDataHandler
 {
-    var completeStory:NSDictionary;
+    let completeStory:NSDictionary;
+    var currentStory:NSDictionary;
+    
     
     init()
     {
+        //  Initialize complete story to pull in the whole pList
+        //  This will help us if we need to reset it by never changing it
         let AdventurePath = Bundle.main.path(forResource: "Adventure.plist", ofType:nil)!;
         self.completeStory = NSDictionary(contentsOfFile:AdventurePath)!;
         
-        print(self.completeStory.description);
+        
+        //  This will be where we currently are in the story with the optoins
+        //  It should start out equal to the completeStory
+        self.currentStory = NSDictionary(dictionary: self.completeStory);
+        
+        
+        //print(self.currentStory.description);
+        startStory()
+
+        advanceStory(choiceKey: "Choice 1");
+        advanceStory(choiceKey: "Choice 1");
+        
         
       //  super.init();
     }
     
-    
-    func getNextPart() -> NSDictionary
+    /*
+    func advanceStory(choiceKey:String) -> NSDictionary
     {
+        
         return ["test":"Testing"] as NSDictionary
     }
+    */
+    
+    func advanceStory(choiceKey:String) -> (Prompt: String, Choice1: String, Choice2: String)
+    {
+        self.currentStory = self.currentStory[choiceKey] as! NSDictionary;
+        
+        print(self.currentStory.description);
+            
+        return ("Prompt", "Choice 1", "Choice 2");
+    }
+    
+    
+    func startStory() -> (Prompt: String, Choice1: String, Choice2: String)
+    {
+        var storyOptionsArray:Array = self.currentStory.allKeys;
+        
+        
+        storyOptionsArray.removeAll(where: { $0 as! String == "Choice 1" })
+        
+        return(self.currentStory["Prompt"] as! String, storyOptionsArray[0] as! String, storyOptionsArray[1] as! String)
+    }
+    
     
 }
--- Assignment4/ViewController.swift
+++ Assignment4/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var PromptLabel:UILabel!;
+    @IBOutlet var Choice1Button:UIButton!;
+    @IBOutlet var Choice2Button:UIButton!;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,7 +21,9 @@ class ViewController: UIViewController {
         
         var test = StoryDataHandler();
         
+        
         print(test.completeStory.description)
+        
         
     }
 
